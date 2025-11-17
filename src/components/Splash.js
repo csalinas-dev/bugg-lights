650aa1f6b10f9a805e0509c6f55f697e3945c93c
Christopher Salinas Jr. <dev@csalinas.dev>
2025-11-16 23:04:59 -0700

    refactor(splash): sizing

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,30 +1,53 @@
+"use client";
+
 import React from 'react';
 import { Box } from '@mui/material';
+import Image from 'next/image';
 
 const Splash = () => {
   return (
     <Box
       sx={{
+        height: 'auto',
         position: 'relative',
         width: '100%',
-        height: '100vh',
-        backgroundImage: 'url(/background-expanded.jpg)',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
       }}
     >
-      <Box
-        component="img"
-        src="/logo.jpg"
-        alt="Logo"
-        sx={{
-          maxWidth: '200px',
-          maxHeight: '200px',
+      <Image
+        src="/background-expanded.jpg"
+        alt="Background"
+        width={6000}
+        height={5000}
+        style={{
+          width: '100%',
+          objectFit: "cover",
+          height: 'auto',
+          minHeight: '60vh',
         }}
+        priority
       />
+      <Box
+        sx={{
+          alignItems: 'top',
+          display: 'flex',
+          height: '100%',
+          justifyContent: 'center',
+          left: 0,
+          position: 'absolute',
+          top: 0,
+          width: '100%',
+        }}
+      >
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={300}
+          height={300}
+          style={{
+            objectFit: 'contain',
+          }}
+        />
+      </Box>
     </Box>
   );
 };

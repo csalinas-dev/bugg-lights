ead1b9dc7b9de8a1f87d419f33722d025e2e8999
Christopher Salinas Jr. <dev@csalinas.dev>
2025-11-22 01:15:14 -0700

    chore: update description

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ const nunito = Nunito({
 
 export const metadata = {
   title: "The Bugg Lights",
-  description: "The Bugg Lights is a free, family-friendly Christmas lights display in Albuquerque, New Mexico. Each night from Thanksgiving to New Year’s, the home comes alive with thousands of glowing lights, holiday characters, and festive scenes that welcome visitors of all ages. On Friday and Saturday evenings, families can enjoy free hot chocolate and occasional visits from Santa Claus. Guests are encouraged to bring non-perishable food items to support Storehouse Albuquerque, helping local families throughout the season. The display is community-driven, warm, and built with the simple goal of spreading joy during the holidays.",
+  description: "The Bugg Lights display is a free, family-friendly Christmas tradition in Albuquerque, New Mexico. Each night from Thanksgiving to New Year’s, the home comes alive with thousands of glowing lights, holiday characters, and festive scenes that welcome visitors of all ages. On Friday and Saturday evenings, families can enjoy free hot chocolate and occasional visits from Santa Claus. Guests are encouraged to bring non-perishable food items to support Storehouse Albuquerque, helping local families throughout the season. The display is community-driven, warm, and built with the simple goal of spreading joy during the holidays.",
 };
 
 export default function RootLayout({ children }) {

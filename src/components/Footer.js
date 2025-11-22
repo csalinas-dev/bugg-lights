6f399d3e9c2084486d7b0b2c91e603d99c3609c7
Christopher Salinas Jr. <dev@csalinas.dev>
2025-11-22 01:04:16 -0700

    refactor(footer): container

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 "use client";
 
 import React from 'react';
-import { Typography, Grid, styled, Stack, Box } from '@mui/material';
+import { Typography, Grid, styled, Stack, Box, Container } from '@mui/material';
 import { faInstagram, faTiktok, faXTwitter, faFacebookF } from '@fortawesome/free-brands-svg-icons';
 import { SocialOrnament } from './SocialOrnament';
 import Image from 'next/image';
 
-const Container = styled('footer')`
+const Footer = styled('footer')`
   background-color: #0A0F1C;
   padding-bottom: 48px;
   padding-top: 98px;
@@ -24,45 +24,40 @@ const Container = styled('footer')`
   }
 `;
 
-const Footer = () => (
-  <Container>
-    <Grid
-      container
-      spacing={4}
-      sx={{
-        maxWidth: '1200px',
-        margin: '0 auto',
-        width: '100%',
-        paddingLeft: '16px',
-        paddingRight: '16px',
-      }}
-      columns={{ xs: 1, md: 3 }}
-    >
-      <Grid size={1} component={Stack} alignItems="center" justifyContent="center">
-        <Image src="/logo.png" width={250} height={250} alt="The Bugg Lights Logo" />
+const Component = () => (
+  <Footer>
+    <Container>
+      <Grid
+        container
+        spacing={4}
+        columns={{ xs: 1, md: 3 }}
+      >
+        <Grid size={1} component={Stack} alignItems="center" justifyContent="center">
+          <Image src="/logo.png" width={250} height={250} alt="The Bugg Lights Logo" />
+        </Grid>
+        <Grid size={1} component={Stack} alignItems="center" justifyContent="center">
+          <Box sx={{ textAlign: { xs: "center", md: "left" } }}>
+            <Typography variant="h4">
+              The Bugg Lights
+            </Typography>
+            <Typography variant="body1">
+              2223 Hoffman Dr. NE
+              <br />
+              Albuquerque, New Mexico 87110
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid size={1} component={Stack} alignItems="center" justifyContent="center">
+          <Stack direction="row" sx={{ gap: '0.25rem' }} justifyContent={{ xs: "center", md: "flex-start" }}>
+            <SocialOrnament href="https://www.instagram.com/bugglights/" icon={faInstagram} color="Yellow" />
+            <SocialOrnament href="https://www.tiktok.com/@bugglights" icon={faTiktok} color="Red" />
+            <SocialOrnament href="https://x.com/BuggLights" icon={faXTwitter} color="Green" />
+            <SocialOrnament href="https://www.facebook.com/people/Bugg-Lights/61584253583434/" icon={faFacebookF} color="Blue" />
+          </Stack>
+        </Grid>
       </Grid>
-      <Grid size={1} component={Stack} alignItems="center" justifyContent="center">
-        <Box sx={{ textAlign: { xs: "center", md: "left" } }}>
-          <Typography variant="h4">
-            The Bugg Lights
-          </Typography>
-          <Typography variant="body1">
-            2223 Hoffman Dr. NE
-            <br />
-            Albuquerque, New Mexico 87110
-          </Typography>
-        </Box>
-      </Grid>
-      <Grid size={1} component={Stack} alignItems="center" justifyContent="center">
-        <Stack direction="row" sx={{ gap: '0.25rem' }} justifyContent={{ xs: "center", md: "flex-start" }}>
-          <SocialOrnament href="https://www.instagram.com/bugglights/" icon={faInstagram} color="Yellow" />
-          <SocialOrnament href="https://www.tiktok.com/@bugglights" icon={faTiktok} color="Red" />
-          <SocialOrnament href="https://x.com/BuggLights" icon={faXTwitter} color="Green" />
-          <SocialOrnament href="https://www.facebook.com/people/Bugg-Lights/61584253583434/" icon={faFacebookF} color="Blue" />
-        </Stack>
-      </Grid>
-    </Grid>
-  </Container>
+    </Container>
+  </Footer>
 );
 
-export default Footer;
+export default Component;

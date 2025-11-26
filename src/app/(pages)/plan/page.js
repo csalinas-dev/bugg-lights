f8f8d94813b427ee4e3a4d226fbdc86cebbe1a79
Christopher Salinas Jr. <dev@csalinas.dev>
2025-11-26 01:57:56 -0700

    feat(plan): santa calendar

--- src/app/(pages)/plan/page.js
+++ src/app/(pages)/plan/page.js
@@ -1,7 +1,11 @@
-import { Container } from "@mui/material";
+import { Box, Container } from "@mui/material";
+import Image from "next/image";
 
 export default function Page() {
   return (
-    <Container maxWidth="xl">Plan Your Visit</Container>
+    <Container maxWidth="xl">
+      <Box>Plan Your Visit</Box>
+      <Image src="/santa-calendar.png" width={450} height={500} alt="The Bugg Lights 2025 Santa Calendar"/>
+    </Container>
   );
 }
